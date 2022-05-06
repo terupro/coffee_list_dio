d23261d919d67afe23b85b2b73c5b4fe8064f140
‘terupro’ <terupro911@gmail.com>
2022-05-06 09:12:55 +0900

    update

--- lib/model/coffee.dart
+++ lib/model/coffee.dart
@@ -1,10 +1,9 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:flutter/foundation.dart';
 
+// build_runnerを使うことで自動生成されるファイル
 part 'coffee.freezed.dart';
 part 'coffee.g.dart';
-
-// APIから取得したい内容のクラスを作成
 
 @freezed
 class Coffee with _$Coffee {
